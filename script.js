6aba5385b3c807da83c7d0855017e1fe66393712
slo-1c-09-2021 <81371213+slo-1c-09-2021@users.noreply.github.com>
2022-02-10 17:28:22 +0100

    we do the uploading
    yeah man

---
+++ script.js
@@ -0,0 +1,63 @@
+function zag1()
+{
+    var zag = document.getElementById("zag1");
+    zag.innerHTML="<img src=\"zag1rozw.png\" width=\"511\" height=\"238\">";
+}
+
+function zag1hide()
+{
+    var zag = document.getElementById("zag1");
+    zag.innerHTML="<img src=\"zag1.png\" width=\"511\" height=\"238\">";
+}
+
+
+function zag2()
+{
+    var zag = document.getElementById("zag2");
+    zag.innerHTML="Trzy";
+}
+
+function zag2hide()
+{
+    var zag = document.getElementById("zag2");
+    zag.innerHTML="Na pastwisko wyruszyło stado krów. Jedna z nich idzie w przodzie przed dwiema, jedna za dwiema i jedna pomiędzy dwiema. Ile krów jest w stadzie?";
+}
+
+
+function zag3()
+{
+    var zag = document.getElementById("zag3");
+    zag.innerHTML="Odpowiedź na pytanie, czy królik przejdzie przez szczelinę między Ziemią a liną, wydłużając linę o jeden metr, brzmi: tak. I to jest coś, co możemy obliczyć matematycznie. Zakładając, że Ziemia jest kulą o promieniu około 63000 km, r = 63000 km, chociaż sznur, który ją całkowicie otacza, musi mieć znaczną długość, rozszerzenie go o jeden metr spowodowałoby powstanie szczeliny około 16 cm. To spowodowałoby, że królik mógłby wygodnie przejść przez szczelinę między obydwoma elementami.";
+}
+
+function zag3hide()
+{
+    var zag = document.getElementById("zag3");
+    zag.innerHTML="Zakładając, że Ziemia jest kulą i przez równik przepuszczamy sznurek w taki sposób, że go nim otaczamy. Jeśli wydłużymy linę o jeden metr, tak aby tworzyła okrąg wokół Ziemi to czy królik może przejść przez szczelinę między Ziemią a liną?";
+}
+
+
+function zag4()
+{
+    var zag = document.getElementById("zag4");
+    zag.innerHTML="Panowie palą wszystkie trzy gatunki papierosów, pani nie pali żadnych.";
+}
+
+function zag4hide()
+{
+    var zag = document.getElementById("zag4");
+    zag.innerHTML="Państwo Kowalscy siedzą przy stole z ich miłym gościem. Mówią właśnie o papierosach i z rozmowy okazuje się, że dwie osoby palą Dukaty, dwie palą Giewonty i dwie Wczasowe. Pani nie pali Wczasowych, Giewontów, a kto nie pali Dukatów, nie pali także Giewontów. Jakie papierosy pali każda z trzech osób?";
+}
+
+
+function zag5()
+{
+    var zag = document.getElementById("zag5");
+    zag.innerHTML="Najpopularniejsza cyfra to 1! Czy wiesz dlaczego? Każda liczba 1-9 pojawia się dokładnie taką samą liczbę razy w każdych dziesięciu liczbach. Ale ponieważ uwzględniliśmy liczbę 1000, występuje dodatkowe wystąpienie liczby 1. W sumie liczba 1 pojawia się 301 razy, a każda inna liczba pojawia się 300 razy.";
+}
+
+function zag5hide()
+{
+    var zag = document.getElementById("zag5");
+    zag.innerHTML="Jaka cyfra pojawia się najczęściej między liczbami od 1 do 1000 włącznie?";
+}
